9d439eefe9d46d6f18f4806be5a4d8ab52d840d2
Valerio Bellizia <valbellix@gmail.com>
2026-04-16 22:37:57 +0200

    Adding CLI11 and its license

---
+++ README.md
@@ -0,0 +1,8 @@
+## License
+
+This project is licensed under the MIT License.
+
+### Third-party dependencies
+
+- CLI11 — BSD-3-Clause License  
+  See `LICENSES/CLI11.txt` for details.
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <string>
+#include <CLI/CLI.hpp>
+
+#include "c8/Emulator.h"
+
 int main(int argc, char* argv[]) {
+    CLI::App cliApp {"Chip-8 emulator/interpreter"};
+    std::string fileName;
+    cliApp.add_option("-f,--file", fileName, "File to run");
+
+    CLI11_PARSE(cliApp, argc, argv);
+
+    Emulator emu;
+    try {
+        emu.init();
+        emu.loadFile(fileName);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
